469802f38c9e97a545794a3ca470d2f560bef6b5
Timothy Fisher <tfisher9180@gmail.com>
2018-01-04 17:29:09 -0700

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+var basePaths = {
+	scss: './sass/',
+	js: './js/',
+	img: './images/',
+	fonts: './fonts/',
+	node: './node_modules/',
+	src: './src/',
+};
+
+var gulp = require('gulp')
+var autoprefixer = require('autoprefixer');
+var postcss = require('gulp-postcss');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var del = require('del');
+var $ = require('gulp-load-plugins')();
+
+gulp.task('css', function() {
+	return gulp.src(basePaths.scss + '**/*.scss')
+	.pipe(sass({
+		outputStyle: 'expanded',
+		indentType: 'tab',
+		indentWidth: '1'
+	}).on('error', sass.logError))
+	.pipe(postcss([
+		autoprefixer('last 2 versions', '> 1%')
+	]))
+	.pipe(gulp.dest('./'));
+});
+
+gulp.task('js', function() {
+	var scripts = [
+		basePaths.js + 'navigation.js',
+		basePaths.js + 'skip-link-focus-fix.js'
+	];
+	return gulp.src(scripts)
+		.pipe(concat('theme.js'))
+		.pipe(gulp.dest(basePaths.js));
+});
+
+gulp.task('fontello', function() {
+	return gulp.src(basePaths.fonts + 'config.json')
+		.pipe($.fontello())
+		.pipe(gulp.dest(basePaths.fonts))
+});
+
+gulp.task('watch', function() {
+	gulp.watch(['./**/*.css', './**/*.scss'], ['css']);
+	gulp.watch([basePaths.js + '**/*.js', '!theme.js'], ['js']);
+});
+
+gulp.task('clean-src', function() {
+	return del([basePaths.src + '**/*']);
+});
+
+gulp.task('default', ['watch', 'js', 'fonts']);
